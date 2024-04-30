6d98ea47181a45f94978ce2b5105ba79743df1aa
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 09:55:22 +0100

    A meal menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import { names } from './data'
 
 function App() {
+  const [detail, setDetail] = useState(names)
+  const [selectedCategory, setSelectedCategory] = useState('all')
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category)
+    if (category === 'all') {
+      setDetail(names)
+    } else {
+      const filteredItems = names.filter((item) => item.category === category)
+      setDetail(filteredItems)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <div className='main-header'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+
+        <div className='btn-container'>
+          <button
+            type='button'
+            className={`btn ${selectedCategory === 'all' ? 'active' : ''}`}
+            onClick={() => handleCategoryClick('all')}
+          >
+            all
+          </button>
+          <button
+            type='button'
+            className={`btn ${
+              selectedCategory === 'breakfast' ? 'active' : ''
+            }`}
+            onClick={() => handleCategoryClick('breakfast')}
+          >
+            breakfast
+          </button>
+          <button
+            type='button'
+            className={`btn ${selectedCategory === 'lunch' ? 'active' : ''}`}
+            onClick={() => handleCategoryClick('lunch')}
+          >
+            lunch
+          </button>
+          <button
+            type='button'
+            className={`btn ${selectedCategory === 'shakes' ? 'active' : ''}`}
+            onClick={() => handleCategoryClick('shakes')}
+          >
+            shakes
+          </button>
+        </div>
+
+        <div className='section-container'>
+          {detail.map((data) => {
+            const { name, price, about, img, index } = data
+            return (
+              <article key={index} className='menu-items'>
+                <img className='img' alt='' src={img} />
+                <div className='item-info'>
+                  <header>
+                    <h5>{name}</h5>
+                    <span className='item-price'>{price}</span>
+                  </header>
+                  <p className='about-item'>{about}</p>
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const names = [
+  {
+    name: 'diner double',
+    price: '$13.99',
+    about:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velitlabore earum obcaecati id sapiente sint odit.',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-2.jpeg',
+    index: 1,
+    category: 'lunch',
+  },
+  {
+    name: 'buttermilk pancake',
+    price: '$15.99',
+    about:
+      'Libero et suscipit ad omnis Optio provident reiciendis praesentium quasi nesciunt hic, at dolores facilis explicabo accusamus architecto quisquam excepturi dolore id',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-1.jpeg',
+    index: 2,
+    category: 'breakfast',
+  },
+  {
+    name: 'godzilla milkshake',
+    price: '$6.99',
+    about:
+      'impedit perspiciatis culpa nesciunt temporibus praesentium eos id nostrum vitae dignissimos, dicta ducimus, voluptates reprehenderit. Amet temporibus suscipit alias voluptatem cum',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-3.jpeg',
+    index: 3,
+    category: 'shakes',
+  },
+  {
+    name: 'country delight',
+    price: '$20.99',
+    about:
+      'Fugiat repudiandae vero sunt, placeat recusandae doloribus deleniti incidunt animi provident minima architecto nobis, sit amet consectetur adipisicing elit. Ea nisi minima eum exercitationem debitis quibusdam ex',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-4.jpeg',
+    index: 4,
+    category: 'breakfast',
+  },
+  {
+    name: 'egg attack',
+    price: '$22.99',
+    about:
+      'ipsa incidunt ab odit illo eos sed eum earum magnam, odio quae. Provident tenetur eligendi doloribus ipsum repellat quaerat consequatur est, labore sed eum. Dolorem molestiae velit consectetur',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-5.jpeg',
+    index: 5,
+    category: 'lunch',
+  },
+  {
+    name: 'oreo dream',
+    price: '$18.99',
+    about:
+      'Fugiat neque possimus provident magni rem dolores veniam voluptas laborum recusandae? Excepturi omnis impedit necessitatibus autem commodi nemo ullam, at amet iure',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-6.jpeg',
+    index: 6,
+    category: 'shakes',
+  },
+  {
+    name: 'bacon overflow',
+    price: '$8.99',
+    about:
+      'consectetur adipisicing elit. Minus fuga rem porro ex consequatur dignissimos quasi, ab culpa, necessitatibus facere nobis aut, quas in cupiditate minima reprehenderit distinctio recusandae reiciendis',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-7.jpeg',
+    index: 7,
+    category: 'breakfast',
+  },
+  {
+    name: 'american classic',
+    price: '$12.99',
+    about:
+      'nam illum repudiandae ratione praesentium pariatur adipisci doloribus assumenda aspernatur nulla amet perspiciatis quibusdam dignissimos ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quae. A exercitationem iste atque esse',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-8.jpeg',
+    index: 8,
+    category: 'lunch',
+  },
+  {
+    name: 'quarantine buddy',
+    price: '$16.99',
+    about:
+      'Maxime nulla, fugit vero minus rerum commodi possimus tempora, suscipit quo, quae explicabo blanditiis ratione iusto quibusdam illo aut quidem repudiandae. Quae, unde odio, aliquam molestias exercitationem sint dolorem nobis ullam consequatur sunt magni amet consectetur adipisicing elit',
+    img: 'https://react-vite-projects-5-menu.netlify.app/images/item-9.jpeg',
+    index: 9,
+    category: 'shakes',
+  },
+]
